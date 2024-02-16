4502b0d8c6c0da7b26646e3f49a6b9fdfeeaf5f9
GustavoS7 <gustavods2003@outlook.com>
2024-02-16 00:10:45 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import { knex } from "./database";
+import fastify from "fastify";
+import { env } from "./env";
+
+const app = fastify();
+
+app.get("/hello", async () => {
+  const transaction = await knex("transactions")
+    .insert({
+      id: crypto.randomUUID(),
+      title: "Transação de teste",
+      amount: 1000,
+    })
+    .returning("*");
+
+  return transaction;
+});
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log("HTTP server is running");
+  });
