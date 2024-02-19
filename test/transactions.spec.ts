d823cd8d9eafa66f387cc004d6798438096738de
GustavoS7 <gustavods2003@outlook.com>
2024-02-18 23:31:12 -0300

    env file for test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, it, describe } from "vitest";
+import { afterAll, beforeAll, it, describe, expect } from "vitest";
 import { app } from "../src/app";
 import request from "supertest";
 
@@ -20,5 +20,29 @@ describe("Transactions routes", () => {
         type: "credit",
       })
       .expect(201);
+  });
+
+  it("Should be able to list all transactions", async () => {
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "any_transaction",
+        amount: 1,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    const listTransactionsResponse = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "any_transaction",
+        amount: 1,
+      }),
+    ]);
   });
 });
